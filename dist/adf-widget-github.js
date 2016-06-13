104c233666d2954f11c3437360fd3fe79f73d53a
Sebastian Sdorra <s.sdorra@gmail.com>
2016-06-13 17:41:49 +0200

    release v0.2.0

--- dist/adf-widget-github.js
+++ dist/adf-widget-github.js
@@ -25,43 +25,65 @@
 
 
 
-angular.module('adf.widget.github', ['adf.provider', 'highcharts-ng'])
+angular
+  .module('adf.widget.github', ['adf.provider', 'highcharts-ng'])
   .value('githubApiUrl', 'https://api.github.com/repos/')
-  .config(["dashboardProvider", function(dashboardProvider){
-    // template object for github widgets
-    var widget = {
-      templateUrl: '{widgetsPath}/github/src/view.html',
-      reload: true,
+  .config(RegisterWidgets);
+
+function RegisterWidgets(dashboardProvider) {
+  // template object for github widgets
+  var widget = {
+    reload: true,
+    category: 'GitHub',
+    controllerAs: 'vm',
+    edit: {
+      templateUrl: '{widgetsPath}/github/src/edit.html'
+    }
+  };
+
+  var commitWidgets = angular.extend({
+    templateUrl: '{widgetsPath}/github/src/view.html',
+    resolve: {
+      /* @ngInject */
+      commits: ["github", "config", function (github, config) {
+        if (config.path) {
+          return github.getCommits(config);
+        }
+      }]
+    }
+  }, widget);
+
+  // register github template by extending the template object
+  dashboardProvider
+    .widget('githubHistory', angular.extend({
+      title: 'Github History',
+      description: 'Display the commit history of a GitHub project as chart',
+      controller: 'GithubHistoryController'
+    }, commitWidgets))
+    .widget('githubAuthor', angular.extend({
+      title: 'Github Author',
+      description: 'Displays the commits per author as pie chart',
+      controller: 'GithubAuthorController'
+    }, commitWidgets))
+    .widget('githubIssues', angular.extend({
+      title: 'Github Issues',
+      description: 'Displays issues as list of a GitHub project',
+      controller: 'GithubIssuesController',
+      templateUrl: '{widgetsPath}/github/src/issues.html',
       resolve: {
-        /* @ngInject */
-        commits: ["githubService", "config", function(githubService, config){
+        issues: function(github, config){
           if (config.path){
-            return githubService.getCommits(config.path);
+            return github.getIssues(config);
           }
-        }]
-      },
-      edit: {
-        templateUrl: '{widgetsPath}/github/src/edit.html'
+        }
       }
-    };
+    }, widget));;
+}
+RegisterWidgets.$inject = ["dashboardProvider"];
 
-    // register github template by extending the template object
-    dashboardProvider
-      .widget('githubHistory', angular.extend({
-        title: 'Github History',
-        description: 'Display the commit history of a GitHub project as chart',
-        controller: 'githubHistoryCtrl'
-        }, widget))
-      .widget('githubAuthor', angular.extend({
-        title: 'Github Author',
-        description: 'Displays the commits per author as pie chart',
-        controller: 'githubAuthorCtrl'
-        }, widget));
-
-  }]);
-
-angular.module("adf.widget.github").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/github/src/edit.html","<form role=form><div class=form-group><label for=path>Github Repository Path</label> <input type=text class=form-control id=path ng-model=config.path placeholder=\"Enter Path (username/reponame)\"></div></form>");
-$templateCache.put("{widgetsPath}/github/src/view.html","<div><div class=\"alert alert-info\" ng-if=!chartConfig>Please insert a repository path in the widget configuration</div><div ng-if=chartConfig><highchart id=chart1 config=chartConfig></highchart></div></div>");}]);
+angular.module("adf.widget.github").run(["$templateCache", function($templateCache) {$templateCache.put("{widgetsPath}/github/src/edit.html","<form role=form><div class=form-group><label for=path>Github Repository Path</label> <input type=text class=form-control id=path ng-model=config.path placeholder=\"Enter Path (username/reponame)\"></div><div class=form-group><label for=path>Access Token</label> <input type=text class=form-control id=path ng-model=config.accessToken></div></form>");
+$templateCache.put("{widgetsPath}/github/src/issues.html","<div><div ng-if=!config.path class=\"alert alert-info\">Please configure the widget</div><div ng-if=config.path><ul class=media-list><li class=media ng-repeat=\"issue in vm.issues\"><div class=media-left><img class=\"media-object img-thumbnail\" ng-src={{issue.user.avatar_url}} style=\"width: 64px; height: 64px;\"></div><div class=media-body><h4 class=media-heading><a href={{issue.html_url}} target=_blank>{{issue.number}} {{issue.title}}</a></h4><p>{{issue.body | limitTo: 128}}</p><small><a href={{issue.user.html_url}}>{{issue.user.login}}</a>, {{issue.created_at | date: \'yyyy-MM-dd HH:mm\'}}</small></div></li></ul></div></div>");
+$templateCache.put("{widgetsPath}/github/src/view.html","<div><div class=\"alert alert-info\" ng-if=!vm.chartConfig>Please insert a repository path in the widget configuration</div><div ng-if=vm.chartConfig><highchart id=chart1 config=vm.chartConfig></highchart></div></div>");}]);
 /*
  * The MIT License
  *
@@ -88,196 +110,269 @@ $templateCache.put("{widgetsPath}/github/src/view.html","<div><div class=\"alert
 
 
 
-angular.module('adf.widget.github')
-  .service('githubService', ["$q", "$http", "githubApiUrl", function($q, $http, githubApiUrl){
-    return {
-      getCommits: function(path){
-        var deferred = $q.defer();
-        var url = githubApiUrl + path + '/commits?callback=JSON_CALLBACK';
-        $http.jsonp(url)
-          .success(function(data){
-            if (data && data.meta){
-              var status = data.meta.status;
-              if ( status < 300 ){
-                deferred.resolve(data.data);
-              } else {
-                deferred.reject(data.data.message);
-              }
-            }
-          })
-          .error(function(){
-            deferred.reject();
-          });
-        return deferred.promise;
-      }
-    };
-  }]);
+angular
+  .module('adf.widget.github')
+  .factory('github', GithubService);
 
-/*
- * The MIT License
- *
- * Copyright (c) 2015, Sebastian Sdorra
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+function GithubService($q, $http, githubApiUrl) {
+  var service = {
+    getCommits: getCommits,
+    getIssues: getIssues
+  };
 
+  return service;
 
+  // implementation
 
-angular.module('adf.widget.github')
-  .controller('githubHistoryCtrl', ["$scope", "config", "commits", function($scope, config, commits){
+  function getIssues(config) {
+    return fetch(createUrl('issues', config));
+  }
 
-    function parseDate(input) {
-      var parts = input.split('-');
-      return Date.UTC(parts[0], parts[1]-1, parts[2]);
+  function getCommits(config) {
+    return fetch(createUrl('commits', config));
+  }
+
+  function createUrl(type, config){
+    var url = githubApiUrl + config.path + '/' + type + '?callback=JSON_CALLBACK';
+    if (config.accessToken){
+      url += '&access_token=' + config.accessToken;
     }
+    return url;
+  }
 
-    var data = {};
-    angular.forEach(commits, function(commit){
-      var day = commit.commit.author.date;
-      day = day.substring(0, day.indexOf('T'));
-
-      if (data[day]){
-        data[day]++;
-      } else {
-        data[day] = 1;
-      }
-    });
-
-    var seriesData = [];
-    angular.forEach(data, function(count, day){
-      seriesData.push([parseDate(day), count]);
-    });
-    seriesData.sort(function(a, b){
-      return a[0] - b[0];
-    });
-
-    if ( commits ){
-      $scope.chartConfig = {
-        chart: {
-          type: 'spline'
-        },
-        title: {
-          text: 'Github commit history'
-        },
-        xAxis: {
-          type: 'datetime'
-        },
-        yAxis: {
-          title: {
-            text: 'Commits'
-          },
-          min: 0
-        },
-        series: [{
-          name: config.path,
-          data: seriesData
-        }]
-      };
-    }
-
-  }]);
-
-/*
- * The MIT License
- *
- * Copyright (c) 2015, Sebastian Sdorra
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-
-
-angular.module('adf.widget.github')
-  .controller('githubAuthorCtrl', ["$scope", "config", "commits", function($scope, config, commits){
-
-    var data = {};
-    angular.forEach(commits, function(commit){
-      var author = commit.commit.author.name;
-      if (data[author]){
-        data[author]++;
-      } else {
-        data[author] = 1;
-      }
-    });
-
-    var seriesData = [];
-    angular.forEach(data, function(count, author){
-      seriesData.push([author, count]);
-    });
-    if (seriesData.length > 0){
-      seriesData.sort(function(a, b){
-        return b[1] - a[1];
+  function fetch(url){
+    var deferred = $q.defer();
+    $http.jsonp(url)
+      .success(function(data) {
+        if (data && data.meta) {
+          var status = data.meta.status;
+          if (status < 300) {
+            deferred.resolve(data.data);
+          } else {
+            deferred.reject(data.data.message);
+          }
+        }
+      })
+      .error(function() {
+        deferred.reject();
       });
-      var s = seriesData[0];
-      seriesData[0] = {
-        name: s[0],
-        y: s[1],
-        sliced: true,
-        selected: true
-      };
-    }
+    return deferred.promise;
+  }
+}
+GithubService.$inject = ["$q", "$http", "githubApiUrl"];
 
-    if ( commits ){
-      $scope.chartConfig = {
-        chart: {
-          plotBackgroundColor: null,
-          plotBorderWidth: null,
-          plotShadow: false
-        },
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+
+angular
+  .module('adf.widget.github')
+  .controller('GithubIssuesController', GithubIssuesController);
+
+function GithubIssuesController(config, issues) {
+  var vm = this;
+
+  vm.issues = issues;
+}
+GithubIssuesController.$inject = ["config", "issues"];
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+
+angular
+  .module('adf.widget.github')
+  .controller('GithubHistoryController', GithubHistoryController);
+
+function GithubHistoryController(config, commits) {
+  var vm = this;
+
+  function parseDate(input) {
+    var parts = input.split('-');
+    return Date.UTC(parts[0], parts[1] - 1, parts[2]);
+  }
+
+  var data = {};
+  angular.forEach(commits, function (commit) {
+    var day = commit.commit.author.date;
+    day = day.substring(0, day.indexOf('T'));
+
+    if (data[day]) {
+      data[day]++;
+    } else {
+      data[day] = 1;
+    }
+  });
+
+  var seriesData = [];
+  angular.forEach(data, function (count, day) {
+    seriesData.push([parseDate(day), count]);
+  });
+  seriesData.sort(function (a, b) {
+    return a[0] - b[0];
+  });
+
+  if (commits) {
+    vm.chartConfig = {
+      chart: {
+        type: 'spline'
+      },
+      title: {
+        text: 'Github commit history'
+      },
+      xAxis: {
+        type: 'datetime'
+      },
+      yAxis: {
         title: {
-          text: config.path
+          text: 'Commits'
         },
-        plotOptions: {
-          pie: {
-            allowPointSelect: true,
-            cursor: 'pointer',
-            dataLabels: {
-              enabled: true,
-              color: '#000000',
-              connectorColor: '#000000',
-              format: '<b>{point.name}</b>: {point.percentage:.1f} %'
-            }
-          }
-        },
-        series: [{
-          type: 'pie',
-          name: config.path,
-          data: seriesData
-        }]
-      };
-    }
+        min: 0
+      },
+      series: [{
+        name: config.path,
+        data: seriesData
+      }]
+    };
+  }
 
-  }]);
+}
+GithubHistoryController.$inject = ["config", "commits"];
+
+/*
+ * The MIT License
+ *
+ * Copyright (c) 2015, Sebastian Sdorra
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+
+
+angular
+  .module('adf.widget.github')
+  .controller('GithubAuthorController', GithubAuthorController);
+
+function GithubAuthorController(config, commits) {
+  var vm = this;
+
+  var data = {};
+  angular.forEach(commits, function (commit) {
+    var author = commit.commit.author.name;
+    if (data[author]) {
+      data[author]++;
+    } else {
+      data[author] = 1;
+    }
+  });
+
+  var seriesData = [];
+  angular.forEach(data, function (count, author) {
+    seriesData.push([author, count]);
+  });
+  if (seriesData.length > 0) {
+    seriesData.sort(function (a, b) {
+      return b[1] - a[1];
+    });
+    var s = seriesData[0];
+    seriesData[0] = {
+      name: s[0],
+      y: s[1],
+      sliced: true,
+      selected: true
+    };
+  }
+
+  if (commits) {
+    vm.chartConfig = {
+      chart: {
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false
+      },
+      title: {
+        text: config.path
+      },
+      plotOptions: {
+        pie: {
+          allowPointSelect: true,
+          cursor: 'pointer',
+          dataLabels: {
+            enabled: true,
+            color: '#000000',
+            connectorColor: '#000000',
+            format: '<b>{point.name}</b>: {point.percentage:.1f} %'
+          }
+        }
+      },
+      series: [{
+        type: 'pie',
+        name: config.path,
+        data: seriesData
+      }]
+    };
+  }
+
+}
+GithubAuthorController.$inject = ["config", "commits"];
 })(window);
